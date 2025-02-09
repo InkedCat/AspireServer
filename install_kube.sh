a3b12cb5f979b97a597c2d5695c9aa38d378304a
InkedCat <146424693+InkedCat@users.noreply.github.com>
2025-02-09 19:41:51 +0100

    feat: scripts more verbose

--- install_kube.sh
+++ install_kube.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 sudo kubeadm init --config ./kubernetes/kubeadm.yaml
+echo "Kubernetes installed !"
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+echo "User kubectl initiated !"
 
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.2/manifests/tigera-operator.yaml
-kubectl create -f ./kubernetes/custom.yaml
+kubectl create -f ./kubernetes/calico/custom.yaml
+echo "Calico CNI installed !"
+
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+echo "Metrics server installed !"
--- uninstall_kube.sh
+++ uninstall_kube.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 sudo kubeadm reset
 sudo rm -r ~/.kube
+echo "Kubernetes removed !"
 
 sudo nft flush ruleset
 sudo firewall-cmd --reload
+echo "Firewall rules flushed !"
 
 sudo rm /etc/cni/net.d/10-calico.conflist
 sudo rm /etc/cni/net.d/calico-kubeconfig
+echo "Calico CNI removed !"
