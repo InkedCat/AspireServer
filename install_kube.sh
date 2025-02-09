029e4d323f07cb40883a309af1ad49f3ec5def20
InkedCat <146424693+InkedCat@users.noreply.github.com>
2025-02-09 19:54:31 +0100

    fead: improve verbosity in scripts

--- install_kube.sh
+++ install_kube.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
+
+echo_success() {
+    echo
+    echo -e "\033[1;32m[SUCCESS]\033[0m $1"
+}
+
 sudo kubeadm init --config ./kubernetes/kubeadm.yaml
-echo "Kubernetes installed !"
+echo_success "Kubernetes installed !"
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-echo "User kubectl initiated !"
+echo_success "User kubectl initiated !"
 
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.2/manifests/tigera-operator.yaml
 kubectl create -f ./kubernetes/calico/custom.yaml
-echo "Calico CNI installed !"
+echo_success "Calico CNI installed !"
 
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
-echo "Metrics server installed !"
+echo_success "Metrics server installed !"
--- uninstall_kube.sh
+++ uninstall_kube.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
+
+echo_success() {
+    echo
+    echo -e "\033[1;32m[SUCCESS]\033[0m $1"
+}
+
 sudo kubeadm reset
 sudo rm -r ~/.kube
-echo "Kubernetes removed !"
+echo_success "Kubernetes removed !"
 
 sudo nft flush ruleset
 sudo firewall-cmd --reload
-echo "Firewall rules flushed !"
+echo_success "Firewall rules flushed !"
 
 sudo rm /etc/cni/net.d/10-calico.conflist
 sudo rm /etc/cni/net.d/calico-kubeconfig
-echo "Calico CNI removed !"
+echo_success "Calico CNI removed !"
